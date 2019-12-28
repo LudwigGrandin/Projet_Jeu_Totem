b59034b4e79b92386a2a8dbc47f9db37c30b7a75
Killian Brondy <43031530+BrKillian@users.noreply.github.com>
2019-12-28 15:11:43 +0100

    Ajout procedure gestions des points
    Ajouts des procédures de getion des points

--- projet/libraryProjet.h
+++ projet/libraryProjet.h
@@ -24,7 +24,7 @@ void JouerCarteTotem(TCarte carte, TPile *pioche ,TJoueur *joueurQuiJoue, TJoueu
 void tete_coyote(TJoueur * j1, TJoueur * j2);//échange le totem avec celui d'un autre joueur
 void tete_corbeau(TJoueur * j1, TJoueur * j2);//échange la main avec celle d'un autre joueur
 
-//Killian
+//Pioche
 void init_sans_doublons(int* tableau,int taille);
 void melanger(int* tableau, int taille);
 //void init_pioche(int* tableau,int nbCarte,TCarte* listeCarte, TPile* pioche);
@@ -33,10 +33,17 @@ void init_ListeCarte(TCarte* liste,int nbCarte,int nbJoueur);
 void afficherCartes(TCarte* listeCartes,int taille);
 int saisir_entre(int min, int max);
 
+//Points
+void ajout_Points(TJoueur* listeJoueur,int nbJoueur);
+void initialiser_Points(TJoueur* listeJoueur,int nbJoueur);
+void affichage_Points(TJoueur* listeJoueur,int nbJoueur);
+
 //Gestion init partie
 void Distribuer_Cartes(TJoueur *listeJoueur, TPile *listeCartes, int nbCartes, int nbJoueur);
 int Choix_Premier_Joueur(int nbJoueur);
 void Defausser_Carte(TMain *mainJoueur, int numCarte);
 
+// Libere la mémoire de toutes les piles et listes chainées des joueurs
+void nettoyage_partie(TJoueur* listeJoueur,int nbJoueur);
 
 #endif
