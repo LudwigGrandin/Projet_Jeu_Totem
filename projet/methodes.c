0713dbf86f617fd55e94a16bda8f818fc0a6da70
Luca <garic.luca@outlook.fr>
2020-01-01 20:44:44 +0100

    zip + pdf final

--- projet/methodes.c
+++ projet/methodes.c
@@ -6,6 +6,56 @@
 
 
 /******************************************** PIOCHE ************************************************/
+
+//procédure permettant de piocher
+void piocher(TMain *mainJoueur, TPile *pile)
+{
+
+    TPilelem *aux;
+    TPilelem *prec;
+    TPilelem *newCell;
+
+    //ALLOUER NewCell
+    newCell = (TPilelem*) malloc(sizeof(TPilelem));
+
+    aux = (*mainJoueur).debut;
+    prec = (*mainJoueur).debut;
+
+    //Si main vide
+   if(est_pile_vide(*pile) != 1)
+    {
+
+        while(aux != NULL)
+        {
+            prec = aux;
+            aux = (*aux).suivant;
+        }
+
+        //Piocher la carte en haut de la pile
+
+        (*newCell).carte = (*(*pile).sommet).carte;
+        depiler(pile);
+
+        //Si mainVide
+        if(aux == prec)
+        {
+            (*newCell).suivant = (*mainJoueur).debut;
+            (*mainJoueur).debut = newCell;
+
+        }
+        else
+        {
+            (*prec).suivant = newCell;
+            (*newCell).suivant = aux;
+        }
+    }
+    else
+    {
+        printf("il n y a plus de cartes dans la pioche\n");
+    }
+
+}
+
 //procédure permettant de remplir un tableau d'entier
 void init_sans_doublons(int* tableau,int taille){
 
@@ -264,9 +314,481 @@ void init_Joueurs(TJoueur * listeJoueur,int nbJoueur){
     }
 }
 
+
 /********************************************** FIN JOUEURS  **********************************************/
 
+/********************************************** GESTION MAIN  **********************************************/
+//Fonction appelée permettant au joueur de jouer une carte
+void JouerCarte(TPile *totem, TPile *pioche, int numCarteMain, TJoueur *joueurQuiJoue, TJoueur *listeJoueur)
+{
 
+    TCarte carte;
+
+    //On récupère l'adresse dans la liste chainée de la carte que le joueur veut jouer
+    carte = Retrait_Carte_Main(&(*joueurQuiJoue).main,numCarteMain);
+
+    //Il faut désallouer l'emplacement qui a été supprimé de la main
+    //Si c'est une carte totem alors on l'ajoute à la pile totem
+    if(carte.type == 1)
+    {
+        empiler(totem,&carte);
+        JouerCarteTotem(carte,pioche,joueurQuiJoue,*listeJoueur);
+        //Mettre condition pour immunité etc ..
+    }
+    else
+    {
+
+    }
+}
+
+//Fonction Permettant de récupérer l'adresse d'une carte contenue dans la liste chaînée de la main
+TCarte Retrait_Carte_Main(TMain *main, int numCarteMain)
+{
+    int trouve = 0, compteur = 1;
+
+    TCarte carte;
+    TPilelem *aux;
+    TPilelem *prec;
+
+    aux = (*main).debut;
+    prec = (*main).debut;
+
+    carte.type=0;
+
+    if((*main).debut == NULL)
+    {
+        printf("Votre main est vide !\n");
+    }
+    else
+    {
+        while(aux != NULL && trouve == 0)
+        {
+            if(compteur == numCarteMain)
+            {
+                trouve = 1;
+            }
+            else
+            {
+                prec = aux;
+                aux = (*aux).suivant;
+
+                compteur ++;
+            }
+        }
+
+        if(trouve == 1)
+        {
+            carte.num = (*aux).carte.num;
+            strcpy(carte.nom,(*aux).carte.nom);
+            strcpy(carte.effet,(*aux).carte.effet);
+            carte.type = (*aux).carte.type;
+            (*prec).suivant=(*aux).suivant;
+            free(aux);
+        }
+    }
+    return carte;
+
+}
+
+//Fonction permettant d'afficher la main d'un joueur
+void Afficher_Main(TMain mainJoueur)
+{
+
+    TPilelem *aux;
+    aux = mainJoueur.debut;
+
+    if(aux == NULL)
+    {
+        printf("vous n'avez pas de cartes en main\n");
+    }
+    else
+    {
+        while(aux != NULL)
+        {
+            printf("\n");
+            printf("carte num = %d\n", (*aux).carte.num);
+            printf("carte nom = %s\n", (*aux).carte.nom);
+            printf("carte effet = %s\n", (*aux).carte.effet);
+            printf("carte type = %d\n", (*aux).carte.type);
+            printf("\n");
+
+            aux = (*aux).suivant;
+        }
+    }
+
+}
+
+//Procédure permettant de jouer une carte Coup Bas. Elle est appellé dans la procédure jouer carte
+
+void JouerCarteCoupBas(TCarte carte, TPile *pioche ,TJoueur *joueurQuiJoue, TJoueur listeJoueur, int *gagnePartie)
+{
+    int i =0;
+     TJoueur *joueurCible;
+    (*joueurQuiJoue).rejouer = 0;
+    (*joueurCible).rejouer = 0;
+    //Montrer la carte à jouer
+    printf("La carte jouee est : %s \n",carte.nom);
+    //Plusieurs actions possibles:
+
+    if(strcmp(carte.nom,"OnArrive") == 0)
+    {
+        //A jouer en même temps qu'un joueur qui gagne
+        //EFFET : Vous gagnez la partie aussi.
+         ajout_Points(joueurQuiJoue, gagnePartie);
+    }
+    else if(strcmp(carte.nom,"EtPaf") == 0)
+    {
+        if((*joueurCible).immunite == 1)
+        {
+            printf("Ce joueur est immunisé contre le vol de totem\n");
+        }
+        else if((*joueurCible).immunite == 1)
+        {
+            printf("Ce joueur est immunisé contre la destruction de totem\n");
+        }
+        else
+        {
+            //Destruction du totem adverse
+            depiler(&(*joueurCible).totem);
+        }
+    }
+    else if(strcmp(carte.nom,"EspritFarceur") == 0)
+    {
+        //EFFET : chacun donne son totem à son voisin de gauche
+   //     Donner_Totem_Vers_Voisin_Gauche(listeJoueur);      
+    }
+    else if(strcmp(carte.nom,"BisonDingo") == 0)
+    {
+        //
+        for(i=0; i<2;i++)
+        {
+            depiler(&(*joueurCible).totem);
+        }
+    }
+    else if(strcmp(carte.nom,"FauxPas") == 0)
+    {
+        int choix = 0;
+        /*Pré-requis : Jouer cette carte durant le tour d’un autre joueur.
+EFFET : Annuler l’action d’un joueur si vous annulez un autre “Faux pas !”. Piochez 2 cartes, sinon rejouez immédiatement.
+*/
+        if(carte.nom == "FauxPas")
+        {
+            printf("Piocher 2 carte en Tapant 1 ou rejouez immédiatement en tapant 2 : \n");
+            scanf("%d", &choix);
+            if(choix == 1)
+            {
+                for(i=0; i<2; i++)
+                {
+                    piocher(&(*joueurQuiJoue).main,pioche);
+                }
+            }
+            else
+            {
+                (*joueurQuiJoue).rejouer = 1;
+            }
+        }
+
+
+    }
+    else if(strcmp(carte.nom,"Pillage") == 0)
+    {
+        TCarte carteTotemAdverse;
+        carteTotemAdverse = (*(*joueurCible).totem.sommet).carte;
+        Depot_Carte_Main(&(*joueurQuiJoue).main,carteTotemAdverse,taille_pile((*joueurQuiJoue).totem));
+        depiler(&(*joueurCible).totem);
+
+
+        if(taille_pile((*joueurQuiJoue).totem) <4)
+        {
+            (*joueurQuiJoue).rejouer = 1;
+        }
+
+        /*
+         Voler et mettez dans votre main le dernière
+          étage du totem d’un joueur. Si votre totem possède
+          moins de 4 étages . Rejouez immédiatement.
+
+        */
+    }
+    else if(strcmp(carte.nom,"EaudeFeu") == 0)
+    {
+        /*
+        EFFET : Piochez 2 cartes puis rejouez immédiatement.
+
+        */
+        for(i=0; i<2;i++)
+        {
+             piocher(&(*joueurQuiJoue).main,pioche);
+        }
+
+        (*joueurQuiJoue).rejouer = 1;
+
+    }
+
+}
+
+//Procédure permettant de défausser une carte
+void Defausser_Carte(TMain *mainJoueur, int numCarte)
+{
+    Retrait_Carte_Main(mainJoueur,numCarte);
+}
+
+//Procédure permettant de donner sa main à son voisin de gauche et de le faire pour tous les joueurs
+void Donner_Main_Vers_Voisin_Gauche(TJoueur *listeJoueur, int nbJoueur)
+{
+    int nbJoueurLocal;
+    TPile totemJoueur0;
+
+    totemJoueur0 = listeJoueur[0].totem;
+
+    for(nbJoueurLocal = 0; nbJoueurLocal < nbJoueur; nbJoueurLocal ++)
+    {
+            listeJoueur[nbJoueurLocal].totem = listeJoueur[nbJoueurLocal+1].totem;
+    }
+
+    listeJoueur[nbJoueur].totem = totemJoueur0;
+}
+
+//Procédure permettant de donner son totem à son voisin de gauche et de le faire pour tous les joueurs
+void Donner_Totem_Vers_Voisin_Gauche(TJoueur *listeJoueur, int nbJoueur)
+{
+    int nbJoueurLocal;
+    TMain mainJoueur0;
+
+    mainJoueur0 = listeJoueur[0].main;
+
+    for(nbJoueurLocal = 0; nbJoueurLocal < nbJoueur; nbJoueurLocal ++)
+    {
+            listeJoueur[nbJoueurLocal].main = listeJoueur[nbJoueurLocal+1].main;
+    }
+
+    listeJoueur[nbJoueur].main = mainJoueur0;
+
+}
+
+// Procédure permettant d'échanger les totems de 2 joueurs
+void tete_coyote(TJoueur * j1, TJoueur * j2)
+{
+    TPile totemint;
+    totemint = (*j1).totem;
+    (*j1).totem = (*j2).totem;
+    (*j2).totem = totemint;
+
+}
+// Procédure permettant déchanger les totems de 2 joueurs
+void tete_corbeau(TJoueur * j1, TJoueur * j2)
+{
+    TMain mainint;
+    mainint = (*j1).main;
+    (*j1).main = (*j2).main;
+    (*j2).main = mainint;
+
+}
+
+//Permet d'échanger les mains de deux TJoueur
+void tete_loup(TMain *mainJoueurQuiJoue,TMain *mainJoueurCible)
+{
+    TMain mainTemp;
+
+    mainTemp = *mainJoueurQuiJoue;
+    mainJoueurQuiJoue = mainJoueurCible;
+    *mainJoueurCible = mainTemp;
+
+
+}
+
+void tete_lynx(TJoueur *joueurQuiJoue,TPile *pioche)
+{
+     int numCarte = 0;
+        TMain main3Cartes;
+        TPilelem *aux;
+        int choix;
+
+        main3Cartes.debut = (TPilelem*) malloc(sizeof(TPilelem*));
+        aux = main3Cartes.debut;
+        //Prenser à retirer ces propriétés quand un autre totem est placé
+        (*joueurQuiJoue).lynx = 1;
+
+        //Permet de tirer 3 cartes
+        for(numCarte=0; numCarte == 3; numCarte ++)
+        {
+            printf("Carte %d : %s", numCarte+1, (*(*pioche).sommet).carte.nom);
+            piocher(&main3Cartes,pioche);
+        }
+
+        printf("Quelle carte choisissez vous ? :");
+        scanf("%d", &choix);
+
+        aux = main3Cartes.debut;
+
+        for(numCarte=0; numCarte<choix-1;numCarte++)
+        {
+            aux = (*aux).suivant;
+        }
+
+        Depot_Carte_Main(&(*joueurQuiJoue).main,(*aux).carte,taille_main((*joueurQuiJoue).main));
+
+        liberer_main(&main3Cartes);
+}
+
+void Depot_Carte_Main(TMain *mainJoueur, TCarte carte, int emplacementMain)
+{
+
+
+    TPilelem *aux, *prec, *newCell;
+    int emplacementMainLocal = 1, trouve = 0;
+
+
+    aux = (*mainJoueur).debut;
+
+    while(aux != NULL && trouve != 1)
+    {
+        if(emplacementMainLocal == emplacementMain)
+        {
+            trouve = 1;
+        }
+        else
+        {
+            prec = aux;
+            aux = (*aux).suivant;
+            emplacementMainLocal ++;
+        }
+    }
+
+    if(trouve == 1)
+    {
+        newCell = (TPilelem*) malloc(sizeof(TPilelem));
+        (*newCell).carte = carte;
+
+        (*prec).suivant = newCell;
+        (*newCell).suivant = aux;
+    }
+    else
+    {
+        printf("veuillez saisir un numéro de carte valide\n");
+    }
+}
+//fonction qui retourne la taille d'une TMain
+int taille_main(TMain mainJoueur)
+{
+    TPilelem *aux;
+    int tailleMain = 0;
+    aux = mainJoueur.debut;
+
+    while(aux != NULL)
+    {
+        aux = (*aux).suivant;
+        tailleMain ++;
+    }
+    return tailleMain;
+}
+//Permet au différentes TCartes d'être jouées
+void JouerCarteTotem(TCarte carte, TPile *pioche ,TJoueur *joueurQuiJoue, TJoueur listeJoueur)
+{
+    int i = 0;
+    TJoueur joueurCible;
+    //Montrer la carte à jouer
+    printf("La carte jouee est : %s \n",carte.nom);
+    //Plusieurs actions possibles:
+    empiler(pioche,&carte);
+    //TeteCoyote
+    //TeteAigle
+    //TeteLoup
+    //TeteCorbeau
+    //TeteLynx
+    //TeteTortue
+    //TeteOurs
+
+    if(strcmp(carte.nom,"TeteCoyote") == 0)
+    {
+        tete_coyote(joueurQuiJoue, &joueurCible);
+    }
+    else if(strcmp(carte.nom,"TeteAigle") == 0)
+    {
+        (*joueurQuiJoue).immunite = 2;
+    }
+    else if(strcmp(carte.nom,"TeteLoup") == 0)
+    {
+        tete_loup(&(*joueurQuiJoue).main,&joueurCible.main);
+    }
+    else if(strcmp(carte.nom,"TeteCorbeau") == 0)
+    {
+        tete_corbeau(joueurQuiJoue,&joueurCible);
+    }
+    else if(strcmp(carte.nom,"TeteLynx") == 0)
+    {
+        tete_lynx(joueurQuiJoue,pioche);
+
+        //Créer une variable dans TJoueur si = 1 alors le joueur piochera 3 fois et défaussera deux cartes à chaque fin tour ?
+    }
+    else if(strcmp(carte.nom,"TeteTortue") == 0)
+    {
+        (*joueurQuiJoue).immunite = 1;
+    }
+    else if(strcmp(carte.nom,"TeteOurs") == 0)
+    {
+        if(joueurCible.immunite == 1)
+        {
+            printf("Ce joueur est immunisé contre la destruction de totem pour l'instant\n");
+        }
+        else
+        {
+            //on détruit un étage du totem adverse
+            depiler(&joueurCible.totem);
+        }
+    }
+}
+
+//fonction permettant de vérifier la présence dans sa main de la carte joué par un joueur 
+int verif_carte(TMain mainJoueur, int numCarte)
+{
+    TPilelem *aux;
+    int trouve = 0;
+
+    aux = mainJoueur.debut;
+
+    if(aux == NULL)
+    {
+        printf("Vous n'avez aucune carte en main");
+    }
+    else
+    {
+        while(aux != NULL && trouve != 1)
+        {
+            if(numCarte == (*aux).carte.num)
+            {
+                trouve = 1;
+            }
+            else
+            {
+                aux = (*aux).suivant;
+            }
+        }
+    }
+    return trouve;
+
+}
+
+// procédure permettant de vider libérer une main de carte entièrement
+void liberer_main(TMain * main)
+{
+    TPilelem *aux;
+    aux = (*main).debut;
+
+    if(aux == NULL){
+        printf("La liste est vide, rien à supprimer");
+    }
+
+    while(aux != NULL){
+
+        (*main).debut = (*aux).suivant;
+        free(aux);
+        aux = (*main).debut;
+    }
+}
+
+
+/********************************************** FIN GESTION MAIN  **********************************************/
 
 /**********************************************  NETTOYAGE MEMOIRE **********************************************/
 //faire appel à cette procédure en fin de partie pour liberer la mémoire
@@ -320,6 +842,9 @@ void affichage_Points(TJoueur* listeJoueur,int nbJoueur){
 
 /**********************************************  FIN POINTS **********************************************/
 
+/**********************************************  GESTION PILE **********************************************/
+
+//fonction retournant une valeur entre 0 et 1 permettant de vérifier si la pile entrée en paramètre est vide ou pas.
 int est_pile_vide(TPile pile)
 {
 	int verif = 0;
@@ -331,7 +856,7 @@ int est_pile_vide(TPile pile)
 	return verif;
 }
 
-
+// procédure permettant d'empiler une carte sur une pile
 void empiler(TPile * pile, TCarte * c)
 {
 	struct TPilelem *aux;
@@ -348,6 +873,8 @@ void empiler(TPile * pile, TCarte * c)
 	(*pile).sommet = newC;
 
 }
+
+// procédure permettant de dépiler le sommet d'une pile
 
 TPile depiler(TPile * pile)
 {
@@ -373,11 +900,15 @@ TPile depiler(TPile * pile)
 	return *pile;
 }
 
+
+// procédure permettant d'initialiser une pile
 void init_pile(TPile * pile)
 {
 	(*pile).sommet = NULL;
 
 }
+
+// procédure permettant d'afficher une pile
 
 void afficher_pile(TPile pile)
 {
@@ -402,6 +933,8 @@ void afficher_pile(TPile pile)
 
 }
 
+//fonction retournant la taille d'une pile
+
 int taille_pile(TPile pile)
 {
 	struct TPilelem *aux;
@@ -422,6 +955,7 @@ int taille_pile(TPile pile)
 	return taille;
 }
 
+//fonction qui dépile une pile jusqu'à ce qu'elle soit vide
 void liberer_pile(TPile * pile)
 {
 
@@ -434,54 +968,51 @@ void liberer_pile(TPile * pile)
 	}
 }
 
-void liberer_main(TMain * main)
+/**********************************************  FIN GESTION PILE **********************************************/
+
+/**********************************************  TOUR JOUEUR **********************************************/
+
+//fonction choisissant le joueur jouant en premier pour la manche en cours.
+
+int Choix_Premier_Joueur(int nbJoueur)
 {
-	TPilelem *aux;
-	aux = (*main).debut;
-
-	if(aux == NULL){
-		printf("La liste est vide, rien à supprimer");
-	}
-
-	while(aux != NULL){
-
-		(*main).debut = (*aux).suivant;
-		free(aux);
-		aux = (*main).debut;
-	}
+    srand(time(NULL));
+    return rand()%nbJoueur+1;
 }
 
+// procédure permettant de distribuer 5 cartes aux joueurs à chaque début de manche
 
-
-int verif_carte(TMain mainJoueur, int numCarte)
+void Distribuer_Cartes(TJoueur *listeJoueur, TPile *listeCartes, int nbCartes, int nbJoueur)
 {
-	TPilelem *aux;
-	int trouve = 0;
+    int nbCartesLocales,nbJoueurLocal = 0;
+    TPilelem *aux;
+    TPilelem *newCell;
 
-	aux = mainJoueur.debut;
-
-	if(aux == NULL)
+    for (nbJoueurLocal = 0; nbJoueurLocal < nbJoueur; nbJoueurLocal ++)
     {
-        printf("Vous n'avez aucune carte en main");
-    }
-    else
-    {
-    	while(aux != NULL && trouve != 1)
-    	{
-            if(numCarte == (*aux).carte.num)
-    		{
-    			trouve = 1;
-    		}
-            else
-            {
-                aux = (*aux).suivant;
-            }
-    	}
-    }
-    return trouve;
 
+        listeJoueur[nbJoueurLocal].main.debut = (TPilelem*) malloc(sizeof(TPilelem));
+        //On met l'adresse de la 1ere carte dans aux
+        aux = listeJoueur[nbJoueurLocal].main.debut;
+        //On tire la 1ere carte pour le 1er emplacement
+        (*aux).carte = (*(*listeCartes).sommet).carte;
+
+        depiler(listeCartes);
+
+        for(nbCartesLocales = 0; nbCartesLocales < 5; nbCartesLocales ++)
+        {
+            newCell = (TPilelem*) malloc(sizeof(TPilelem));
+            (*newCell).carte = (*(*listeCartes).sommet).carte;
+            depiler(listeCartes);
+
+            (*aux).suivant = newCell;
+             aux = (*aux).suivant;
+             (*aux).suivant = NULL;
+        }
+    }
 }
 
+// Procédure gérant le tour d'un joueur
 
 void tour_joueur(TJoueur *listeJoueur, TPile *pioche, int *gagneManche, int *gagnePartie)
 {
@@ -572,3 +1103,6 @@ void tour_joueur(TJoueur *listeJoueur, TPile *pioche, int *gagneManche, int *gag
 
 
 }
+
+
+/********************************************** FIN TOUR JOUEUR **********************************************/
